0383fd316bf76a11561200a93e7a21ff14ff1acc
Manohar1481 <pullagurlamanoharreddy@gmail.com>
2025-04-21 00:42:05 +0530

    updated cidr's

--- outputs.tf
+++ outputs.tf
@@ -3,15 +3,15 @@ output "vpc_id" {
   
 }
 
-output "public_subnet_ids" {
+output "public_subnet_cidrs" {
   value = aws_subnet.public[*].id
 }
 
-output "private_subnet_ids" {
+output "private_subnet_cidrs" {
   value = aws_subnet.private[*].id
 }
 
-output "database_subnet_ids" {
+output "database_subnet_cidrs" {
   value = aws_subnet.database[*].id
 }
 
